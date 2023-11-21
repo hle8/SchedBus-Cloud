fcc561f66b39ad1bf5facdc78ced32ab5ffd292f
Huy Le <A00298028@mycambrian.ca>
2023-11-21 12:50:10 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SchedBus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
