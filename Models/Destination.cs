5d5b292d64587ed6ef493359060c754ae6788496
GamNguyen14 <A00256548@mycambrian.ca>
2023-11-25 09:57:02 -0500

    - Added Destination class
    - Added Property Name, Longtitude and Latitude in Destination class

---
+++ Models/Destination.cs
@@ -0,0 +1,18 @@
+﻿namespace SchedBus.Models;
+
+/* The Destination class represent the plan destination.
+	*	Property:
+	*	- Name			| The location that users want to go or save
+	*	- Longtitude	| The location logntitude 
+	*	- Latitude		| The location latitude
+
+*/
+
+public class Destination
+{
+	public string Name{ get; set; }
+	
+	public double Longitude { get; set; }
+
+	public double Latitude { get; set; }
+}
