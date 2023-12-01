32603d3f0f906c245c7530a4571e311afa338ddd
hle8 <hle8@mycambrian.ca>
2023-11-30 20:58:38 -0500

    Create UI for PlanEditPage  - Create Destination  - Create Arrival time  - Create on/off switch button for choosen plan  - Create add more plan button  - Create Save and Delete button

---
+++ ViewModels/PlansViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SchedBus.Models;
+using SchedBus.Services;
+using System.Collections.ObjectModel;
+
+namespace SchedBus.ViewModels;
+
+public class PlansViewModel : ObservableRecipient
+{
+    private PlanDataService _dataService;
+    private Plan _selectedActivity;
+
+    public ObservableCollection<Plan> Plans => _dataService.Plans;
+
+    public IRelayCommand GoToAddPlanCommand { get; }
+    public IRelayCommand GoToEditPlanCommand { get; }
+
+    public PlansViewModel()
+    {
+        _dataService = PlanDataService.Instance;
+
+        GoToAddPlanCommand = new RelayCommand(GoToAddPlan);
+        GoToEditPlanCommand = new RelayCommand<Plan>(GoToEditPlan);
+    }
+
+    private async void GoToAddPlan()
+    {
+        await Shell.Current.GoToAsync("planeditpage");
+    }
+
+    private void GoToEditPlan(Plan plan)
+    {
+
+    }
+}
