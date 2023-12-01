63eeb978186228ec09c33726f1bf3b9da19fbfe7
hle8 <hle8@mycambrian.ca>
2023-11-30 20:58:38 -0500

    Create UI for PlanEditPage  - Create Destination  - Create Arrival time  - Create on/off switch button for choosen plan  - Create add more plan button  - Create Save and Delete button

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace SchedBus
+﻿using SchedBus.Pages;
+
+namespace SchedBus
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("planeditpage", typeof(PlanEditPage));
         }
     }
 }
