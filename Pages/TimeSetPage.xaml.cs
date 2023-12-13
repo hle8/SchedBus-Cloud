38b675069b5f615c7592903ae77c18c594e65fa7
GamNguyen14 <A00256548@mycambrian.ca>
2023-12-13 17:00:51 -0500

    Do the spinner for the timer

--- Pages/TimeSetPage.xaml.cs
+++ Pages/TimeSetPage.xaml.cs
@@ -1,9 +1,41 @@
-namespace SchedBus.Pages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SchedBus.Models;
+using SchedBus.ViewModels;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui;
+using System.Collections.ObjectModel;
 
-public partial class TimeSetPage : ContentPage
+namespace SchedBus.Views
 {
-	public TimeSetPage()
-	{
-		InitializeComponent();
-	}
+    public partial class TimeSetPage : ContentPage
+    {
+        private TimeSetViewModel? _viewModel;
+
+        public TimeSetPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new TimeSetViewModel();
+        }
+
+        public ObservableCollection<TimeSet> TimeSets { get; set; }
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Shell.SetBackButtonBehavior(this, new BackButtonBehavior
+            {
+                Command = new AsyncRelayCommand(async () =>
+                {
+                    await Shell.Current.GoToAsync($"..");
+                })
+            });
+        }
+    }
 }
--- ViewModels/TimeSetViewModel.cs
+++ ViewModels/TimeSetViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SchedBus.Models;
+using System.Collections.ObjectModel;
 
 namespace SchedBus.ViewModels;
 
@@ -8,14 +9,21 @@ public class TimeSetViewModel : ObservableObject, IQueryAttributable
 {
     public TimeSet TimeSet { get; set; }
 
+    public ObservableCollection<string> HourItemsSource { get; set; }
+    public ObservableCollection<string> MinuteItemsSource { get; set; }
+    public ObservableCollection<string> AMPMItemsSource { get; set; }
+
     public IAsyncRelayCommand SaveCommand { get; }
     public IAsyncRelayCommand DeleteCommand { get; }
 
     public TimeSetViewModel()
     {
         TimeSet = new TimeSet();
-        SaveCommand = new AsyncRelayCommand(Save);
-        DeleteCommand = new AsyncRelayCommand(Delete);
+        HourItemsSource = new ObservableCollection<string>(Enumerable.Range(1, 12).Select(h => h.ToString()));
+        MinuteItemsSource = new ObservableCollection<string>(Enumerable.Range(0, 60).Select(m => m.ToString("00")));
+        AMPMItemsSource = new ObservableCollection<string>(new[] { "AM", "PM" });
+        SaveCommand = new AsyncRelayCommand(() => Save(SelectedTimeSet));
+        DeleteCommand = new AsyncRelayCommand(() => Delete(SelectedTimeSet));
     }
 
     void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
@@ -28,13 +36,22 @@ public class TimeSetViewModel : ObservableObject, IQueryAttributable
         }
     }
 
-    public async Task Save()
+    public TimeSet SelectedTimeSet
+    {
+        get => TimeSet;
+        set
+        {
+            TimeSet = value;
+            OnPropertyChanged(nameof(SelectedTimeSet));
+        }
+    }
+    public async Task Save(TimeSet timeSet)
     {
         //var timeSetId = await SqliteDataStore.SaveTimeSetAsync(_timeset, _planId);
         await Shell.Current.GoToAsync($"..");
     }
 
-    public async Task Delete()
+    public async Task Delete(TimeSet timeSet)
     {
         //await SqliteDataStore.RemoveNotUsedTimeSetAsync(_timeset.Id, _planId);
         await Shell.Current.GoToAsync($"..");
