7b7d62caa28bb0d1b689216e97123771020c0c11
hle8 <hle8@mycambrian.ca>
2023-12-13 18:05:18 -0500

    Fix InitializeComponent method of TimeSetPage

--- Pages/TimeSetPage.xaml.cs
+++ Pages/TimeSetPage.xaml.cs
@@ -1,41 +1,9 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using SchedBus.Models;
-using SchedBus.ViewModels;
-using CommunityToolkit.Mvvm.Input;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui;
-using System.Collections.ObjectModel;
+namespace SchedBus.Pages;
 
-namespace SchedBus.Views
+public partial class TimeSetPage : ContentPage
 {
-    public partial class TimeSetPage : ContentPage
-    {
-        private TimeSetViewModel? _viewModel;
-
-        public TimeSetPage()
-        {
-            InitializeComponent();
-
-            BindingContext = new TimeSetViewModel();
-        }
-
-        public ObservableCollection<TimeSet> TimeSets { get; set; }
-        private void InitializeComponent()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            Shell.SetBackButtonBehavior(this, new BackButtonBehavior
-            {
-                Command = new AsyncRelayCommand(async () =>
-                {
-                    await Shell.Current.GoToAsync($"..");
-                })
-            });
-        }
-    }
+	public TimeSetPage()
+	{
+		InitializeComponent();
+	}
 }
